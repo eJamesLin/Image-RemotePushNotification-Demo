cbd4527a787ef98aa6d24baa94293a0db78a685a
CJ Lin <ejames.lin@gmail.com>
2016-08-31 00:23:04 +0800

    Initial Commit

---
+++ Push/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Push
+//
+//  Created by CJ Lin on 2016/8/31.
+//  Copyright © 2016年 CJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Push/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Push
+//
+//  Created by CJ Lin on 2016/8/31.
+//  Copyright © 2016年 CJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
